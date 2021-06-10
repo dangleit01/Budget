2a173ab07d4fd1091eaef52a83fb7654656ac46f
dang.le <dang.le@vtl-vtl.com.vn>
2021-06-10 11:30:32 +0700

    Format for budget value

--- app/src/main/java/com/th1b0/budget/features/budgetform/BudgetFormActivity.java
+++ app/src/main/java/com/th1b0/budget/features/budgetform/BudgetFormActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import com.th1b0.budget.R;
 import com.th1b0.budget.databinding.ActivityBudgetFormBinding;
 import com.th1b0.budget.model.Budget;
+import com.th1b0.budget.util.CurrencyUtil;
 import com.th1b0.budget.util.DataManager;
 
 /**
@@ -51,6 +54,88 @@ public final class BudgetFormActivity extends AppCompatActivity implements Budge
 
     setupToolbar();
     fillForm();
+    setupListener();
+  }
+
+  private void setupListener() {
+
+    mView.addingToNumber.thousand.setOnClickListener(v -> {
+      if(mView.value.getText().length() > 0) {
+        String cleanString = mView.value.getText().toString().toString().replaceAll("[$,.]", "");
+        double parsed = Double.parseDouble(cleanString)/100;
+        String formatted = CurrencyUtil.formatToUSD(parsed*1000);
+        mView.value.setText(formatted);
+        mView.value.setSelection(formatted.length());
+        //double dValue = Double.parseDouble(CurrencyUtil.removeCurrencySymbol(mView.value.getText().toString())) * 1000;
+        //mView.value.setText(CurrencyUtil.formatToUSD(dValue));
+      }
+    });
+
+    mView.addingToNumber.hundredThousand.setOnClickListener(v -> {
+      if(mView.value.getText().length() > 0) {
+        String cleanString = mView.value.getText().toString().toString().replaceAll("[$,.]", "");
+        double parsed = Double.parseDouble(cleanString)/100;
+        String formatted = CurrencyUtil.formatToUSD(parsed*100000);
+        mView.value.setText(formatted);
+        mView.value.setSelection(formatted.length());
+        //double dValue = Double.parseDouble(CurrencyUtil.removeCurrencySymbol(mView.value.getText().toString())) * 1000;
+        //mView.value.setText(CurrencyUtil.formatToUSD(dValue));
+      }
+    });
+
+    mView.addingToNumber.hundred.setOnClickListener(v -> {
+      if(mView.value.getText().length() > 0) {
+        String cleanString = mView.value.getText().toString().toString().replaceAll("[$,.]", "");
+        double parsed = Double.parseDouble(cleanString)/100;
+        String formatted = CurrencyUtil.formatToUSD(parsed*100);
+        mView.value.setText(formatted);
+        mView.value.setSelection(formatted.length());
+        //double dValue = Double.parseDouble(CurrencyUtil.removeCurrencySymbol(mView.value.getText().toString())) * 1000;
+        //mView.value.setText(CurrencyUtil.formatToUSD(dValue));
+      }
+    });
+
+    mView.addingToNumber.ten.setOnClickListener(v -> {
+      if(mView.value.getText().length() > 0) {
+        String cleanString = mView.value.getText().toString().toString().replaceAll("[$,.]", "");
+        double parsed = Double.parseDouble(cleanString)/100;
+        String formatted = CurrencyUtil.formatToUSD(parsed*10);
+        mView.value.setText(formatted);
+        mView.value.setSelection(formatted.length());
+        //double dValue = Double.parseDouble(CurrencyUtil.removeCurrencySymbol(mView.value.getText().toString())) * 1000;
+        //mView.value.setText(CurrencyUtil.formatToUSD(dValue));
+      }
+    });
+
+    mView.value.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+      }
+      private String current = "";
+      @Override
+      public void afterTextChanged(Editable s) {
+        if(!s.toString().equals(current)){
+          mView.value.removeTextChangedListener(this);
+
+          String cleanString = s.toString().replaceAll("[$,.]", "");
+
+          double parsed = Double.parseDouble(cleanString)/100;
+          String formatted = CurrencyUtil.formatToUSD(parsed);
+          //String formatted = NumberFormat.getCurrencyInstance().format((parsed/100));
+
+          current = formatted;
+          mView.value.setText(formatted);
+          mView.value.setSelection(formatted.length());
+          mView.value.addTextChangedListener(this);
+        }
+      }
+    });
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,7 +187,9 @@ public final class BudgetFormActivity extends AppCompatActivity implements Budge
   private void fillForm() {
     mView.title.setText(mBudget.getTitle());
     if (mBudget.getValue() != 0) {
-      mView.value.setText(String.valueOf(mBudget.getValue()));
+      String formatted = CurrencyUtil.formatToUSD(mBudget.getValue());
+      mView.value.setText(formatted);
+      //mView.value.setText(String.valueOf(mBudget.getValue()));
     }
   }
 
@@ -130,7 +217,10 @@ public final class BudgetFormActivity extends AppCompatActivity implements Budge
   private void updateBudgetFromForm() {
     mBudget.setTitle(mView.title.getText().toString());
     try {
-      mBudget.setValue(Double.parseDouble(mView.value.getText().toString()));
+      String cleanString = mView.value.getText().toString().toString().replaceAll("[$,.]", "");
+      double parsed = Double.parseDouble(cleanString)/100;
+      mBudget.setValue(parsed);
+      //mBudget.setValue(Double.parseDouble(mView.value.getText().toString()));
     } catch (NumberFormatException e) {
       // Nothing
     }
